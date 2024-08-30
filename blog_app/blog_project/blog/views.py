6c2fbe1408324b2276d7261d12687eef127b43c2
“Ebenezeri <cebendemalek@gmail.com>
2024-08-30 09:22:40 +0300

    Blog App

---
+++ blog_app/blog_project/blog/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+# Create your views here.
