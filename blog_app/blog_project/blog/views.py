30df6b866a27788d6f97a53781e6db6b0fb66509
“Ebenezeri <cebendemalek@gmail.com>
2024-09-02 11:34:41 +0300

    [ADD]Delete functionality

--- blog_app/blog_project/blog/views.py
+++ blog_app/blog_project/blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import Post
 
@@ -23,3 +25,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
